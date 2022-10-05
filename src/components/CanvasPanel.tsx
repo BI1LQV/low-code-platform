68303a1c3e98e178efdc5666b33f5f786cd85bf2
bi1lqv <bi1lqv.y@gmail.com>
2022-10-05 16:17:04 +0800

    feat: dsl reactive化

--- src/components/CanvasPanel.tsx
+++ src/components/CanvasPanel.tsx
@@ -1,26 +1,29 @@
-import type { Ref } from "vue"
-import { defineComponent, ref } from "vue"
+import type { ComputedRef, Ref } from "vue"
+import { computed, defineComponent, reactive, ref } from "vue"
 import Slots from "@/slots"
 export default defineComponent(() => {
-  const dsl: Record<string, string>[] = [
+  const dsl: Record<string, string>[] = reactive([
     { type: "input", id: "1" },
     { type: "input", id: "2" },
     { type: "input", id: "3" },
-  ]
-  let compList: (JSX.Element)[] = []
+  ])
+
   let binderList: Map<string, Ref<any>> = new Map([
     ["1", ref(1)],
     ["2", ref(1)],
     ["3", ref(1)],
   ])
-  for (const { type, id } of dsl) {
-    const Element = Slots[(type as "input")](binderList.get(id)!, {}, {})
-    compList.push(<Element></Element>)
-  }
-
+  let compList: ComputedRef<(JSX.Element)[]> = computed(() => {
+    let list = []
+    for (const { type, id } of dsl) {
+      const Element = Slots[(type as "input")](binderList.get(id)!, {}, { height: "200px" })
+      list.push(<Element></Element>)
+    }
+    return list
+  })
   return () => (
     <div w-800px border-3px>
-     { compList }
+     { compList.value }
     </div>
   )
 })
--- src/slots/Input.tsx
+++ src/slots/Input.tsx
@@ -2,11 +2,13 @@ import type { Ref } from "vue"
 import { defineComponent } from "vue"
 import type { Options, Styles } from "@/models"
 
-export default function Input(binder: Ref<any>, _prop: Options, _style: Styles) {
+export default function Input(binder: Ref<any>, prop: Options, style: Styles) {
   return defineComponent(() => () => (
     <div>
       <input class="border-1 border-black" type="text"
         v-model={binder.value}
+        style={style}
+        props={prop}
        />
        <div>{binder.value}</div>
     </div>
