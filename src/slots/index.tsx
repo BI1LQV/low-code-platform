9e3b0ba6e415dea9a11e16c6131bc7393aeea7e3
bi1lqv <bi1lqv.y@gmail.com>
2022-10-07 16:37:29 +0800

    refactor: 去掉styles

--- src/slots/index.tsx
+++ src/slots/index.tsx
@@ -5,14 +5,11 @@ import { allSlots } from "@/models"
 let c: Record<string, typeof Slot> = import.meta.glob("./*.tsx", { eager: true })
 
 export const Slots: Map<allSlotsKey, typeof Slot["Component"] > = new Map()
-export const Styles: Map<allSlotsKey, typeof Slot["Style"] > = new Map()
 export const Props: Map<allSlotsKey, typeof Slot["Prop"] > = new Map()
 
-Object.values(c).forEach(({ Component, Style, Prop }) => {
+Object.values(c).forEach(({ Component, Prop }) => {
   // @ts-expect-error let me do
   Slots.set(allSlots[Component.name], Component)
-  // @ts-expect-error let me do
-  Styles.set(allSlots[Component.name], Style)
   // @ts-expect-error let me do
   Props.set(allSlots[Component.name], Prop)
 })
