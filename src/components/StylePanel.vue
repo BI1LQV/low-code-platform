21ad38feb7ac61861c1383f718473e92fcce7857
bi1lqv <bi1lqv.y@gmail.com>
2022-12-01 21:39:35 +0800

    feat: 样式里增加边距

--- src/components/StylePanel.vue
+++ src/components/StylePanel.vue
@@ -21,6 +21,18 @@ const { selectedProp: prop } = toRefs(props)
         <el-form-item label="高度">
           <NumberWithUnit v-model="prop.style.height" :units="['%', 'px']"></NumberWithUnit>
         </el-form-item>
+        <el-form-item label="上间距">
+          <NumberWithUnit v-model="prop.style['margin-top']" :units="['%', 'px']"></NumberWithUnit>
+        </el-form-item>
+        <el-form-item label="下间距">
+          <NumberWithUnit v-model="prop.style['margin-bottom']" :units="['%', 'px']"></NumberWithUnit>
+        </el-form-item>
+        <el-form-item label="左间距">
+          <NumberWithUnit v-model="prop.style['margin-left']" :units="['%', 'px']"></NumberWithUnit>
+        </el-form-item>
+        <el-form-item label="右间距">
+          <NumberWithUnit v-model="prop.style['margin-right']" :units="['%', 'px']"></NumberWithUnit>
+        </el-form-item>
         <Component :is="props.optionalPanel" :prop="prop"></Component>
       </el-form>
     </div>
