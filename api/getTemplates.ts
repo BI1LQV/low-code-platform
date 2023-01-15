d3f61d16a84ce33e1fbc8f2022cd45f02ce99003
bi1lqv <bi1lqv.y@gmail.com>
2023-01-15 14:28:27 +0800

    fix: 再次尝试serverless

--- api/getTemplates.ts
+++ api/getTemplates.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node"
-import { db } from "./db"
+import { db } from "./_db"
 export default async (request: VercelRequest, response: VercelResponse) => {
   (await db).query("SELECT * FROM test_1").then((res) => {
     response.status(200).send(JSON.stringify(res[0]))
