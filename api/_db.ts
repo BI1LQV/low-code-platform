2c0b4960c1a7ef08d6f094eeec1726f708be2d21
bi1lqv <bi1lqv.y@gmail.com>
2023-01-15 14:33:06 +0800

    fix: 修不好了 不修了

--- api/_db.ts
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2/promise"
-export const db = mysql.createConnection({
-  host: process.env.DB_HOST as string,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER as string,
-  password: process.env.DB_PASSWD as string,
-  database: process.env.DB_NAME as string,
-})
--- api/getTemplates.ts
+++ api/getTemplates.ts
@@ -1,7 +1,13 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node"
-import { db } from "./_db"
+import mysql from "mysql2/promise"
 export default async (request: VercelRequest, response: VercelResponse) => {
-  (await db).query("SELECT * FROM test_1").then((res) => {
+  (await mysql.createConnection({
+    host: process.env.DB_HOST as string,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER as string,
+    password: process.env.DB_PASSWD as string,
+    database: process.env.DB_NAME as string,
+  })).query("SELECT * FROM test_1").then((res) => {
     response.status(200).send(JSON.stringify(res[0]))
   })
 }
