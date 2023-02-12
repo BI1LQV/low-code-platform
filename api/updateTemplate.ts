9987f601c0bc042ff1a774f39f0a92e8881089c8
BI1LQV <bi1lqv.y@gmail.com>
2023-02-12 15:15:39 +0800

    refactor: 修正接口与gbcall相同

--- api/updateTemplate.ts
+++ api/updateTemplate.ts
@@ -5,7 +5,7 @@ export default async (request: VercelRequest, response: VercelResponse) => {
   const data = request.body
 
   if (isNaN(Number(id))) {
-    response.status(200).send({ status: "ERR", data: "invalid id" })
+    response.status(200).send({ status: "ERR", res: "invalid id" })
     return
   }
 
@@ -21,11 +21,11 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     ))
     .then(([res]: any) => {
       if (res.affectedRows > 0) {
-        response.status(200).send({ status: "OK", data: 0 })
+        response.status(200).send({ status: "OK", res: 0 })
       } else {
         throw new Error("no such template")
       }
     }).catch((err) => {
-      response.status(200).send({ status: "ERR", data: JSON.stringify(err?.stack) || "db error" })
+      response.status(200).send({ status: "ERR", res: JSON.stringify(err?.stack) || "db error" })
     })
 }
