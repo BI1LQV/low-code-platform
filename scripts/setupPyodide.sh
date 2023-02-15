339cb9904ca5f72f8842f98dd602e17250a2dbc5
bi1lqv <bi1lqv.y@gmail.com>
2023-02-15 19:23:33 +0800

    fix: 只下载了core

--- scripts/setupPyodide.sh
+++ scripts/setupPyodide.sh
@@ -1,10 +1,8 @@
 #! bash
-cd ./node_modules/
-if [ ! -f ./.pyodide.tar.bz2 ]; then
-  echo "download pyodide.tbz2"
-  curl -L -o .pyodide.tar.bz2 https://github.com/pyodide/pyodide/releases/download/0.22.1/pyodide-core-0.22.1.tar.bz2
+if [ "$(uname)" != "Darwin" ]; then
+  echo "download and extract pyodide"
+  curl -L -o .pyodide.tar.bz2 https://github.com/pyodide/pyodide/releases/download/0.22.1/pyodide-0.22.1.tar.bz2
+  tar -jxf .pyodide.tar.bz2 -C public/
 else
-  echo "pyodide.tbz2 exsist"
+  echo "mac please install pyodide yourself"
 fi
-
-tar -jxf .pyodide.tar.bz2 -C ../public/
