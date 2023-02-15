589d0d3da402378f64596f8f6df71aca9404cb24
bi1lqv <bi1lqv.y@gmail.com>
2023-02-15 15:15:03 +0800

    fix: 继续修复ci

--- scripts/setupPyodide.sh
+++ scripts/setupPyodide.sh
@@ -2,7 +2,7 @@
 if [ ! -d ./node_modules/.pyodide ]; then
   echo "download pyodide"
   cd ./node_modules/
-  curl -o .pyodide.tar.bz2 https://github.com/pyodide/pyodide/releases/download/0.22.1/pyodide-0.22.1.tar.bz2
+  curl -L -o .pyodide.tar.bz2 https://github.com/pyodide/pyodide/releases/download/0.22.1/pyodide-0.22.1.tar.bz2
   tar -jxf .pyodide.tar.bz2
   rm .pyodide.tar.bz2
   cd ..
