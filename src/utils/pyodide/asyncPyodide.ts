33d3fdc9ca1f896f520beb4b1bdb5bc2c050f35d
bi1lqv <bi1lqv.y@gmail.com>
2023-02-16 11:55:05 +0800

    fix: 打包问题

--- src/utils/pyodide/asyncPyodide.ts
+++ src/utils/pyodide/asyncPyodide.ts
@@ -1,15 +1,19 @@
+import type { Remote } from "comlink"
 import { wrap } from "comlink"
 import { ref } from "vue"
 import type { PyodideWorker } from "./PyodideWorker"
 
 export const loaded = ref(false)
 type ConstructorTypeOf<T> = new (...args: any[]) => T
-const _PyodideWorker = wrap<ConstructorTypeOf<PyodideWorker>>(
-  new Worker(new URL("./PyodideWorker", import.meta.url), { type: "module" }),
-)
-export const worker = await new _PyodideWorker()
+
+// eslint-disable-next-line import/no-mutable-exports
+export let worker: Remote<PyodideWorker>
 
 export async function load() {
+  const _PyodideWorker = wrap<ConstructorTypeOf<PyodideWorker>>(
+    new Worker(new URL("./PyodideWorker", import.meta.url), { type: "module" }),
+  )
+  worker = await new _PyodideWorker()
   return worker._load().then(() => loaded.value = true)
 }
 
