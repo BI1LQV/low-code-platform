07ee04b8cc3e88dbe02bb613bb0c11e2b248d6aa
bi1lqv <bi1lqv.y@gmail.com>
2023-03-17 14:23:03 +0800

    feat: 提取一些公共配置

--- src/components/StylePanel.vue
+++ src/components/StylePanel.vue
@@ -36,6 +36,28 @@ const { selectedProp: prop } = toRefs(props)
         <el-form-item label="字体大小">
           <NumberWithUnit v-model="prop.style['font-size']" :units="['px', 'rem']"></NumberWithUnit>
         </el-form-item>
+        <el-form-item label="调整大小">
+          <ElRadioGroup v-model="prop.style.resize" class="override-el-radio-button">
+            <ElRadioButton label="both">都可</ElRadioButton>
+            <ElRadioButton label="horizontal">水平</ElRadioButton>
+            <ElRadioButton label="vertical">垂直</ElRadioButton>
+            <ElRadioButton label="none">不可</ElRadioButton>
+          </ElRadioGroup>
+        </el-form-item>
+        <ElFormItem label="背景色">
+          <ElColorPicker v-model="prop.style.backgroundColor" />
+        </ElFormItem>
+        <ElFormItem label="文字颜色">
+          <ElColorPicker v-model="prop.style.color" />
+        </ElFormItem>
+        <ElFormItem label="溢出处理">
+          <ElRadioGroup v-model="prop.style.overflow">
+            <ElRadioButton label="hidden">隐藏</ElRadioButton>
+            <ElRadioButton label="scroll">滚动</ElRadioButton>
+            <ElRadioButton label="visible">直接显示</ElRadioButton>
+          </ElRadioGroup>
+        </ElFormItem>
+
         <Component :is="props.optionalPanel" :prop="prop"></Component>
       </el-form>
     </div>
--- src/slots/ELabel.tsx
+++ src/slots/ELabel.tsx
@@ -1,6 +1,5 @@
 import type { Ref } from "vue"
-import { Fragment, defineComponent, ref } from "vue"
-import { ElColorPicker, ElFormItem, ElRadioButton, ElRadioGroup } from "element-plus"
+import { defineComponent, ref } from "vue"
 
 import type { CommonOptions, SlotOptions } from "@/models/slots"
 import { BaseStyleImpl } from "@/models/slots"
@@ -38,27 +37,3 @@ export const AttrPanel = defineComponent({
   },
 })
 
-export const StylePanel = defineComponent({
-  props: ["prop"],
-  setup(p) {
-    return () => {
-      return (
-        <Fragment>
-          <ElFormItem label="背景色">
-          <ElColorPicker v-model={p.prop.style.backgroundColor} />
-          </ElFormItem>
-          <ElFormItem label="文字颜色">
-          <ElColorPicker v-model={p.prop.style.color} />
-          </ElFormItem>
-          <ElFormItem label="溢出处理">
-            <ElRadioGroup v-model={p.prop.style.overflow}>
-              <ElRadioButton label="hidden">隐藏</ElRadioButton>
-              <ElRadioButton label="scroll">滚动</ElRadioButton>
-              <ElRadioButton label="visible">直接显示</ElRadioButton>
-            </ElRadioGroup>
-          </ElFormItem>
-        </Fragment>
-      )
-    }
-  },
-})
